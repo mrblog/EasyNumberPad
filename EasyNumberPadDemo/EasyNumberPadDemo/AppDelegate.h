34e53acb221cbb8cd99a586d50c2b67e0a26754b
David Beckemeyer <david@Davids-MacBook-Pro.local>
2016-02-10 09:36:46 -0800

    baseline

---
+++ EasyNumberPadDemo/EasyNumberPadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyNumberPadDemo
+//
+//  Created by David Beckemeyer on 2/10/16.
+//  Copyright © 2016 BDT.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
